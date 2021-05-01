121d053f5a666fa004af7b71881d985578c7ff80
Lev Chelyadinov <leva181777@gmail.com>
2021-05-01 20:35:54 +0300

    [WIP] Set up the server information
    Co-Authored-By: Anna Boronina <annwhoorma@users.noreply.github.com>

--- src/Entrypoint.hs
+++ src/Entrypoint.hs
@@ -2,20 +2,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Entrypoint (run) where
 
+import           Data.Text           (pack)
+import           Data.Version        (showVersion)
 import           Language.LSP.Server
+import           Language.LSP.Types
+import qualified Paths_haskell_pyls
 import           RIO
 import           Types               (App)
 
 handlers :: Handlers (LspM ())
 handlers = mempty
 
+serverName :: String
+serverName = "haskell-pyls"
+
+serverVersion :: String
+serverVersion = showVersion Paths_haskell_pyls.version
+
+thisServerInfo :: ServerInfo
+thisServerInfo = ServerInfo (pack serverName) (Just $ pack serverVersion)
+
 run :: RIO App Int
 run = do
-  logInfo "Starting the language server"
+  --logInfo ("Starting " ++ serverName ++ "(v" ++ serverVersion ++ ")")
   liftIO $ runServer $ ServerDefinition
-    { onConfigurationChange = const $ pure $ Right ()
+    { defaultConfig = ()
+    , onConfigurationChange = const $ pure $ Right ()
     , doInitialize = \env _req -> pure $ Right env
     , staticHandlers = handlers
     , interpretHandler = \env -> Iso (runLspT env) liftIO
-    , options = defaultOptions
+    , options = defaultOptions { serverInfo = Just thisServerInfo }
     }
